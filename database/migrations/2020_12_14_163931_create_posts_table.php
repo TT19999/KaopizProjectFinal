c4726f48ae06331b668d0525dac38be5de522eb5
unknown <tunghsut99@gmail.com>
2020-12-15 00:45:28 +0700

    'authozi'

---
+++ database/migrations/2020_12_14_163931_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->text('title');
+            $table->text('content');
+            $table->string('status')->nullable();
+            $table->string('author');
+            $table->string('category')->nullable();
+            $table->text('cover')->nullable();
+            $table->integer('views')->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,53 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('123456'),
+                'created_at' => now(),
+                'updated_at' => now(),
+                'email_verified_at' => now(),
+            ],
+            [
+                'name' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => bcrypt('123456'),
+                'created_at' => now(),
+                'updated_at' => now(),
+                'email_verified_at' => now(),
+            ],
+        ]);
+
+        DB::table('permissions')->insert([
+            ['name' => 'review_post'],
+            ['name' => 'update_post'],
+            ['name' => 'delete_post'],
+            ['name' => 'restore_post'],
+            ['name' => 'force_delete_post'],
+        ]);
+
+        DB::table('roles')->insert([
+            ['name' => 'admin'],
+            ['name' => 'user']
+        ]);
+        DB::table('role_user')->insert(
+            [
+                'role_id' => 1,
+                'user_id' => 1,
+            ],
+            [
+                'role_id'=>2,
+                'user_id'=>2
+            ]
+        );
+        DB::table('permission_role')->insert([
+            ['permission_id' => 1, 'role_id' => 1],
+            ['permission_id' => 2, 'role_id' => 1],
+            ['permission_id' => 3, 'role_id' => 1],
+            ['permission_id' => 4, 'role_id' => 1],
+            ['permission_id' => 5, 'role_id' => 1],
+        ]);
     }
 }
